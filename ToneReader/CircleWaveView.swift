4aa363bd4ed5ec447432f912aee9566b36899c00
Grady Woodruff <jgradywoodruffiv@gmail.com>
2019-12-04 13:58:28 -0500

    Clean up and document

--- ToneReader/CircleWaveView.swift
+++ ToneReader/CircleWaveView.swift
@@ -23,12 +23,24 @@ class CircleWaveView: EZAudioPlot {
             path.move(to: CGPoint(x: centerX + baseRadius, y: centerY))
             
             for i in (0..<pointCount) {
+                /// Create a mirrorIndex, for example:
+                /// If mirrorAmount is set to 50, the mirror index will be 0
+                /// on point 974, and will be 49 by 1024
+                ///
+                /// NOTE: I'm trying to figure out whether it should be subtracting
+                /// 1023 or 1024 in this mirrorIndex equation
                 let mirrorIndex = ((Int(i) + mirrorAmount) - 1023) - 1
                 var radius: Double
                 
+                /// I don't think just adding 1 is how to solve this, but
                 guard let point = points?[Int(i)+1] else { continue }
                 
                 radius = baseRadius + (Double(point.y) * scale)
+                
+                /// Once we get to the last points, and the mirrorIndex is 0 or above,
+                /// as explained above, we start averaging the radius with its original point
+                /// and its mirror point, slowly increasing the percentage towards the mirror point
+                /// until it reaches 100% mirrored by the last point
                 if mirrorIndex >= 0 {
                     let percentage = ((100/Double(mirrorAmount)) * Double(mirrorIndex))/100
                     radius = (radius * (1 - percentage)) + (mirrorPoints[mirrorIndex] * percentage)
@@ -39,6 +51,8 @@ class CircleWaveView: EZAudioPlot {
                 let x = centerX + radius * cos(radians)
                 let y = centerY + radius * sin(radians)
                     
+                /// Add radius to the beginning of the mirrorPoints array
+                /// if the index is below the mirror amount
                 if i < mirrorAmount {
                     mirrorPoints.insert(radius, at: 0)
                 }
